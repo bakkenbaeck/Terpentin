e8a718b807a6fe03236c5c3b016738b7d401447e
Igor Ranieri Elland <igor@elland.me>
2016-10-25 10:42:12 +0200

    Add Xcode project

---
+++ Terpentin/Terpentin/Terpentin.h
@@ -0,0 +1,19 @@
+//
+//  Terpentin.h
+//  Terpentin
+//
+//  Created by Igor Ranieri on 25/10/2016.
+//  Copyright © 2016 Bakken & Bæck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Terpentin.
+FOUNDATION_EXPORT double TerpentinVersionNumber;
+
+//! Project version string for Terpentin.
+FOUNDATION_EXPORT const unsigned char TerpentinVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Terpentin/PublicHeader.h>
+
+
